291e831d53a69198e2ee629d68cea5eee199292e
Davide Ginelli <ginellidavide@gmail.com>
2022-11-16 11:09:46 +0100

    First commit

---
+++ app/src/main/java/it/unimib/firebasecrashlyticstest/MainActivity.java
@@ -0,0 +1,22 @@
+package it.unimib.firebasecrashlyticstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            String s = null;
+            s.charAt(0); // Bug injection
+        });
+    }
+}
